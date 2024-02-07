3470c6586fb01a8eabb6e924d258c7a131d29e47
Rishikdk <rishikhadk6180@gmail.com>
2024-02-07 08:44:32 +0545

    init

---
+++ php/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+<style>
+
+::-webkit-scrollbar {
+    display: none;
+}
+
+
+
+.home{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap:25px;
+}
+
+
+  </style>
+</head>
+<body>
+<div class="home">
+<?php 
+require_once("header.php");
+?>
+
+
+<?php 
+require_once("home.php");
+?>
+
+
+
+
+<?php 
+require_once("footer.php");
+?>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
